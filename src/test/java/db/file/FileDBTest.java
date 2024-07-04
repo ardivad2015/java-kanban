4bbd646531fa9d3da543f17ca0e0d1bf867f3a7c
Ardivad <ardivad@gmail.com>
2024-07-04 04:12:23 +0300

    maven framework, tests

---
+++ src/test/java/db/file/FileDBTest.java
@@ -0,0 +1,43 @@
+package db.file;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileDBTest {
+
+    @Test
+    void loadFromFileNoErrorReturnDataTransfer() {
+        try {
+            final File file = File.createTempFile("fileTaskManager", ".tmp");
+            assertEquals(FileDB.loadFromFile(file).getClass(), FileDB.DataTransfer.class);
+            file.delete();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    void loadFromInvalidFileThrowsIOException() {
+        final File file = new File("notexist");
+        assertThrows(IOException.class, () -> FileDB.loadFromFile(file));
+    }
+
+    @Test
+    void writeToFileCreateNewFile() {
+        final File file = new File(System.getProperty("java.io.tmpdir") + "\\fileBackedTaskManager.tmp");
+        assertFalse(file.exists());
+        FileDB.DataTransfer dataTransfer = new FileDB.DataTransfer();
+        try {
+            FileDB.writeToFile(dataTransfer,file);
+            assertTrue(file.exists());
+            file.delete();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
